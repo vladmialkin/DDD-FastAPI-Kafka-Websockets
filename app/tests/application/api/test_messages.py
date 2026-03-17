9b66448b767ea4c3df26fdf4f2eeec6cd056f8bc
mialkin <mialkin@bolid.ru>
2026-03-17 16:52:23 +0300

    feat: Добавление тестов для api создания чата.

---
+++ app/tests/application/api/test_messages.py
@@ -0,0 +1,53 @@
+from faker import Faker
+from fastapi import FastAPI
+from fastapi import status
+from httpx import Response
+import pytest
+from fastapi.testclient import TestClient
+
+
+@pytest.mark.asyncio
+async def test_create_chat_success(
+        app: FastAPI,
+        client: TestClient,
+        faker: Faker,
+):
+    url = app.url_path_for('create_chat_handler')
+    title = faker.text()[:100]
+    response: Response = client.post(url=url, json={"title": title})
+
+    assert response.is_success
+    json_data = response.json()
+
+    assert json_data["title"] == title
+
+
+@pytest.mark.asyncio
+async def test_create_chat_fail_text_too_long(
+        app: FastAPI,
+        client: TestClient,
+        faker: Faker,
+):
+    url = app.url_path_for('create_chat_handler')
+    title = faker.text(max_nb_chars=1000)[:500]
+    response: Response = client.post(url=url, json={"title": title})
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST, response.json()
+    json_data = response.json()
+
+    assert json_data["detail"]["error"]
+
+
+@pytest.mark.asyncio
+async def test_create_chat_fail_text_empty(
+        app: FastAPI,
+        client: TestClient,
+        faker: Faker,
+):
+    url = app.url_path_for('create_chat_handler')
+    response: Response = client.post(url=url, json={"title": ""})
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST, response.json()
+    json_data = response.json()
+
+    assert json_data["detail"]["error"]
