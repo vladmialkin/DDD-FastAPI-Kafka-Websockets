12bd61c85bda3ea656973aa97bd8022bfc14f9db
mialkin <mialkin@bolid.ru>
2026-03-17 16:33:44 +0300

    fix: Исправление тестов.

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,17 +1,21 @@
+from punq import Container
 from pytest import fixture
 
-from infra.repositories.messages import BaseChatRepository, MemoryChatRepository
-from logic import Mediator, init_mediator
+from infra.repositories.messages import BaseChatRepository
+from logic import Mediator
+from tests.fixtures import init_dummy_container
 
 
 @fixture(scope="function")
-def chat_repository():
-    return MemoryChatRepository()
+def container() -> Container:
+    return init_dummy_container()
 
 
 @fixture(scope="function")
-def mediator(chat_repository: BaseChatRepository) -> Mediator:
-    mediator = Mediator()
-    init_mediator(mediator, chat_repository=chat_repository)
+def mediator(container: Container) -> Mediator:
+    return container.resolve(Mediator)
 
-    return mediator
+
+@fixture(scope="function")
+def chat_repository(container: Container) -> BaseChatRepository:
+    return container.resolve(BaseChatRepository)
